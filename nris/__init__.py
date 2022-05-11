0a049a971e2afb055833115704b502e5b1d3dd25
Igor Kuvaldin <kuvich@gmail.com>
2022-05-11 08:55:54 +0300

    - поправлен баг с русскими именами в отчете НРИС

--- nris/__init__.py
+++ nris/__init__.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from openpyxl import load_workbook
 import sqlite3
 from flask import render_template, Blueprint
@@ -31,8 +32,9 @@ def home():
         file = form.file.data
         start_data = form.start.data
         end_data = form.end.data
+        extension = "." + file.filename.split(".")[-1]
         file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), UPLOAD_FOLDER,
-                                 secure_filename(file.filename))
+                                 secure_filename(str(uuid.uuid4()) + extension))
         file.save(file_path)
         d = EmailList(file_path)
         res = d.list_pyxl(start_data, end_data)
@@ -48,8 +50,6 @@ class EmailList:
         self.file_path = file_path
 
     def list_pyxl(self, data_start, data_end):
-        self.data_start = data_start
-        self.data_end = data_end
         new_xls_email_list = []
         wb = load_workbook(self.file_path)
         for sheet in wb.worksheets:
@@ -59,14 +59,13 @@ class EmailList:
                     data_email = str(column[one_email].value)
                     if '@' in data_email:
                         new_xls_email_list.append(data_email)
-        print(self.data_end)
 
         sql = "','".join(new_xls_email_list)
         self.cursor.execute(f"""SELECT
                                     username, sum(amount) as sum
                                     FROM f_lk_payments
                                     WHERE username in ('{sql}')
-                                    and time > ('{self.data_start}') and time < ('{self.data_end}')
+                                    and time > ('{data_start}') and time < ('{data_end}')
                                     group by username
                                     """)
         res = self.cursor.fetchall()
